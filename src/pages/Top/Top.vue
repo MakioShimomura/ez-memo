3d8d5ff4ab8b2e4efff6e4d311a1778daa49a44d
mshimomura <mshimomura@fourmix.co.jp>
2021-10-22 09:37:45 +0900

    20211021分変更

---
+++ src/pages/Top/Top.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="top">
+    <div class="glow-bg">
+      <div class="glow"></div>
+    </div>
+  </div>
+  <logo />
+</template>
+
+<script>
+import Logo from "../../components/Logo/Logo";
+export default {
+  name: "Top",
+  components: {Logo}
+}
+</script>
+
+<!-- sco
+    LogopedをつけるとTopのみで使える -->
+<style src="./Top.scss" lang="scss" scoped />
